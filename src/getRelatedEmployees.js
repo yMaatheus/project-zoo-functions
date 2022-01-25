ecea9088514b50a775cfa185dfe639428475e824
Matheus <stvgamer74@gmail.com>
2022-01-25 16:28:02 -0300

    R04: Funcao isManager e getRelatedEmployees feita!

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.reduce((acc, { firstName, lastName, managers }) =>
+    (managers.includes(managerId) ? acc.concat([`${firstName} ${lastName}`]) : acc), []);
 }
 
 module.exports = { isManager, getRelatedEmployees };
