a17b800666c1710084c5985957d3b4e7cb4bf357
Matheus <stvgamer74@gmail.com>
2022-01-25 18:49:58 -0300

    R05: Funcao countAnimals feita!

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (typeof animal === 'object') {
+    const specieName = animal.specie;
+    const animalSex = animal.sex;
+    const specieResidents = data.species.find(({ name }) => specieName === name).residents;
+    return specieResidents.reduce((acc, { sex }) =>
+      (animalSex === undefined || sex === animalSex ? acc + 1 : acc), 0);
+  }
+  const objectExit = {};
+  species.forEach(({ name, residents }) =>
+    (Object.assign(objectExit, { [name]: residents.reduce((count) => count + 1, 0) })));
+  return objectExit;
 }
 
 module.exports = countAnimals;
