55f0010d12ebcc3d939225c4e81ce5c9124db707
Matheus <stvgamer74@gmail.com>
2022-01-25 16:12:02 -0300

    R03: Funcao getEmployeeByName feita!

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName)
+    || {}; // Se o find for undefined(falsy) então será: {}
 }
 
 module.exports = getEmployeeByName;
