efea34ed8041a09673a5c60365d5890fca9f2d45
Matheus <stvgamer74@gmail.com>
2022-01-25 15:41:55 -0300

    R01: funcao getSpeciesByIds feita!

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter(({ id }) => ids.includes(id));
 }
 
 module.exports = getSpeciesByIds;
