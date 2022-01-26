a67164a4e2fd2410e5614bfbf6580e6f4cf9eb63
Matheus <stvgamer74@gmail.com>
2022-01-26 16:08:04 -0300

    R06: Funcoes countEntrants e CalculeEntry concluidas

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  const object = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      object.child += 1;
+    } else if (entrant.age < 50) {
+      object.adult += 1;
+    } else {
+      object.senior += 1;
+    }
+  });
+  return object;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) {
+    return 0;
+  }
+  const count = Object.entries(countEntrants(entrants));
+  return count.reduce((acc, current) => acc + data.prices[current[0]] * current[1], 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
