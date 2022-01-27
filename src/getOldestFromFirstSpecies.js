5f429fa5383b65d4bbc483fc9aca1753a42af257
Matheus <stvgamer74@gmail.com>
2022-01-27 18:27:26 -0300

    R09: Funcao getOldestFromFirstSpecies concluida

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find(({ id: employeeID }) => employeeID === id);
+  const firstSpecieID = employee.responsibleFor[0];
+  const specie = species.find(({ id: specieID }) => specieID === firstSpecieID);
+  const oldestAnimal = specie.residents.reduce((acc, animal) =>
+    (animal.age > acc.age ? animal : acc));
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
