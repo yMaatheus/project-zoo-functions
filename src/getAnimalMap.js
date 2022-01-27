222867d05aa758fc289cfd10daf1ad88f345b148
Matheus <stvgamer74@gmail.com>
2022-01-27 15:25:40 -0300

    R07: Funcao getAnimalMap concluida! #VQV

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function callback(options, specieName) {
+  return (animalAcc, { name: animalName, sex: animalSex }) => {
+    if (!Object.keys(animalAcc).includes(specieName)) {
+      Object.assign(animalAcc, { [specieName]: [] });
+    }
+    if (options.sex === undefined || options.sex === animalSex) {
+      animalAcc[specieName].push(animalName);
+      if (options.sorted !== undefined) {
+        animalAcc[specieName].sort();
+      }
+    }
+    return animalAcc;
+  };
+}
+
+function createAnimalMap(options) {
+  return species.reduce((acc, { name: specieName, location, residents }) => {
+    if (!Object.keys(acc).includes(location)) {
+      Object.assign(acc, { [location]: [] });
+    }
+    acc[location].push(residents.reduce(callback(options, specieName), {}));
+    return acc;
+  }, {});
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (typeof options === 'object' && Object.keys(options).includes('includeNames')) {
+    return createAnimalMap(options);
+  }
+  return species.reduce((acc, { name, location }) => {
+    if (!Object.keys(acc).includes(location)) {
+      Object.assign(acc, { [location]: [] });
+    }
+    acc[location].push(name);
+    return acc;
+  }, {});
 }
 
 module.exports = getAnimalMap;
