f1df1bb7abec5bc18dd445eb531aa830652ea824
Matheus <stvgamer74@gmail.com>
2022-01-30 20:16:07 -0300

    R10: Funcao getEmployeesCoverage concluida, projeto finalizado!

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeeByName(name) {
+  return name !== undefined ? employees
+    .find(({ firstName, lastName }) => firstName === name || lastName === name) || null : null;
+}
+
+function getEmployeeById(id) {
+  return id !== undefined ? employees
+    .find(({ id: employeeId }) => employeeId === id) || null : null;
+}
+
+function getSpecies(responsibleFor) {
+  return species.reduce((acc, specie) => {
+    if (responsibleFor.includes(specie.id)) {
+      acc.names.push(specie.name);
+      acc.locations.push(specie.location);
+    }
+    return acc;
+  }, { names: [], locations: [] });
+}
+
+function getEmployeesCoverage(object) {
+  if (typeof object === 'object') {
+    if (getEmployeeByName(object.name) !== null) {
+      const { id, firstName, lastName, responsibleFor } = getEmployeeByName(object.name);
+      const { names, locations } = getSpecies(responsibleFor);
+      return { id, fullName: `${firstName} ${lastName}`, species: names, locations };
+    }
+    if (getEmployeeById(object.id) !== null) {
+      const { id, firstName, lastName, responsibleFor } = getEmployeeById(object.id);
+      const { names, locations } = getSpecies(responsibleFor);
+      return { id, fullName: `${firstName} ${lastName}`, species: names, locations };
+    }
+    throw new Error('Informações inválidas');
+  }
+  return employees.map(({ id, firstName, lastName, responsibleFor }) => {
+    const { names, locations } = getSpecies(responsibleFor); // { names: [], locations: [] }
+    return { id, fullName: `${firstName} ${lastName}`, species: names, locations };
+  });
 }
 
 module.exports = getEmployeesCoverage;
